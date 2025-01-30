ca92e9ae176c79f9f224a78b1389b083747fcf2b
Lucky <lucky123@gmail.com>
2025-01-30 19:53:36 +0530

    AuthModel setup

---
+++ app/components/AuthModel.tsx
@@ -0,0 +1,29 @@
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import Image from "next/image";
+import Logo from "@/public/logo.svg"
+import Github from '@/public/github.svg'
+import Google from "@/public/google.svg"
+export function AuthModel (){
+    return(
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button className=" font-bold">Try For Free</Button>
+            </DialogTrigger>
+            <DialogContent className=" sm:max-w-[360px]">
+            <DialogTitle></DialogTitle>
+<DialogHeader className=" flex  items-center gap-2 flex-row justify-center">
+    <Image src={Logo} alt=" logo" className=" size-10" />
+    <h4 className=' text-3xl font-semibold'>
+                Ca<span className='text-primary'>Le</span>
+            </h4>
+</DialogHeader>
+
+<div className=" flex flex-col mt-5  gap-3">
+    <Button className=" gap-8">  <Image src={Google} alt="Google" className=" size-8" /> Sign In With Google</Button>
+    <Button className=" gap-8"> <Image src={Github} alt="Github" className=" size-8" />Sign In With Github</Button>
+</div>
+            </DialogContent>
+        </Dialog>
+    )
+}
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 
 import Link from "next/link";
 import Logo from  "@/public/logo.svg"
+import { AuthModel } from './AuthModel';
 export function Navbar(){
     return(
         <div className=" flex py-5 items-center justify-between">
@@ -11,8 +12,7 @@ export function Navbar(){
                 Ca<span className='text-blue-500'>Le</span>
             </h4>
             </Link>
-
-  <button>Get Started Today</button>
+<AuthModel/>
 
         </div>
     )
