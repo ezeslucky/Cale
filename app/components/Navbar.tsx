b97d5f201bf8eb209e3945fa915db4fece3ab909
Lucky <lucky123@gmail.com>
2025-01-30 19:26:02 +0530

    shach Ui add

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,12 +4,15 @@ import Link from "next/link";
 import Logo from  "@/public/logo.svg"
 export function Navbar(){
     return(
-        <div className=" flex py-5 items-center justify-normal">
-            <Link href="/">
+        <div className=" flex py-5 items-center justify-between">
+            <Link href="/" className=' flex items-center gap-2 '>
             <Image src={Logo} alt="logo" className="size-10" />
-           
-            
+            <h4 className=' text-3xl font-semibold'>
+                Ca<span className='text-blue-500'>Le</span>
+            </h4>
             </Link>
+
+  <button>Get Started Today</button>
 
         </div>
     )
