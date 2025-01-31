7d705fea33e3df9dc1e7b8137558199b7607f6ce
Lucky <lucky123@gmail.com>
2025-01-31 09:40:27 +0530

    layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,12 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
 import { ReactNode } from "react";
+import Logo from "@/public/logo.svg"
 
 export default function DashboardLayout (
     {children} : {children:ReactNode}
 ){
     return(
         <>
-            <div className=" ">
+            <div className=" min-h-screen w-full grid md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+<div className=" hidden md:block border-r bg-muted/40 ">
+<div className=" flex h-full max-h-screen flex-col gap-2">
+<div className=" flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+<Link href="/"  className=" flex items-center gap-2">
+<Image src={Logo} alt=" logo" className="size-8" />
+<p className=" text-xl font-bold">
+Ca<span className="text-primary">Le</span>
+</p>
+</Link>
+</div>
+</div>
 
+</div>
             </div>
         </>
 
