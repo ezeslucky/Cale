b2fd8a607341eee5e036a2a65fb1ac4144bdfbe3
Lucky <lucky123@gmail.com>
2025-02-01 06:54:09 +0530

    db

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,14 +1,14 @@
 import { PrismaClient } from "@prisma/client"
 
-const prismaClientsingleton = () =>{
+const prismaClientSingleton = () =>{
     return new PrismaClient()
 }
 
 declare const globalThis :{
-    prismaGlobal : ReturnType<typeof prismaClientsingleton>
+    prismaGlobal : ReturnType<typeof prismaClientSingleton>
 } & typeof global
 
-const prisma = globalThis.prismaGlobal ?? prismaClientsingleton()
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
 
 export default prisma
 
